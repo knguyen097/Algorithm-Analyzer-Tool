a5b773f8fe05140f806ce4dcdc133403ed3bed35
knguyen097 <knguyen097@gmail.com>
2025-02-24 22:06:22 -0800

    Update matplot.py

--- matplot.py
+++ matplot.py
@@ -1,29 +1,30 @@
 import algorithm
 import UI_file
 
+    amtInput = #user input from UI_FILE
     execution_times = []
-    num_students = 1000
-    students = [(f"Student{i}", random.randint(1, 2000)) for i in range(num_students)]
+    num_students = amtInput // 3
+    students = [(f"Student{i}", random.randint(amtInput)) for i in range(num_students)]
     execution_times.append(measure_time(bubble_sort, students))
 
-    cards = [random.randint(1, 5000) for _ in range(2000)]
+    cards = [random.randint(amtInput) for _ in range(amtInput // 3)]
     execution_times.append(measure_time(insertion_sort, cards))
 
-    arr = [random.randint(1, 5000) for _ in range(2000)]
+    arr = [random.randint(amtInput) for _ in range(amtInput // 3)]
     execution_times.append(measure_time(merge_sort, arr))
 
-    arr = [random.randint(1, 5000) for _ in range(2000)]
+    arr = [random.randint(amtInput) for _ in range(amtInput // 3)]
     start_time = time.time()
     quick_sort(arr)
     execution_times.append(time.time() - start_time)
 
-    num_books = 2000
-    books = [(f"Book{i}", random.randint(1, 5000)) for i in range(num_books)]
+    num_books = amtInput // 3
+    books = [(f"Book{i}", random.randint(amtInput)) for i in range(num_books)]
     execution_times.append(measure_time(selection_sort, books))
 
-    arr = [random.randint(1, 5000) for _ in range(2000)]
+    arr = [random.randint(amtInput) for _ in range(amtInput // 3)]
     execution_times.append(measure_time(lsd_radix_sort, arr))
-    arr = [random.randint(1, 5000) for _ in range(2000)]
+    arr = [random.randint(amtInput) for _ in range(amtInput // 3)]
     start_time = time.time()
     msd_radix_sort(arr)
     execution_times.append(time.time() - start_time)
