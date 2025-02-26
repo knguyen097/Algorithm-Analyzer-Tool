94ecd51efb3126a1b116999922b29a907151e59a
karl-orquita <karl.orquita@csu.fullerton.edu>
2025-02-25 23:45:58 -0800

    Added a text box entry to fill array and other buttons that will be needed

--- UI_file.py
+++ UI_file.py
@@ -7,13 +7,30 @@ import algorithm
 def buildWindow():
     window = TK.Tk()
     window.geometry("793x393")
-    frm = ttk.Frame(window, padding=155, borderwidth=5, relief="ridge")
-    frm.grid()
-    frm.grid_rowconfigure(0, weight=1)
-    frm.grid_columnconfigure(0, weight=1)
     window.title("Time Complexity Evaluator")
-    TK.Button(frm, text="Quit", command=window.destroy).grid(column=3, row=9)
-    TK.Button(frm, text="Start!",command=window.destroy).grid(column=6, row=5) # window.destroy needs to change so that it starts the algorithmn checking
+    # Framing for entry fields 
+
+    entry_Frm = ttk.Frame(window)
+    entry_Frm.pack(fill="both", expand=True, padx=5, pady=5)
+
+    # Framing for buttons
+    button_Frm = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
+
+    button_Frm.pack()
+    
+    
+    
+    # Buttons of GUI
+
+    quit_Button = TK.Button(button_Frm, text="Quit", command=window.destroy)
+    quit_Button.grid(column=3, row=9)
+    start_Button = TK.Button(button_Frm, text="Start!",command=window.destroy)
+    start_Button.grid(column=6, row=5) 
+    create_RandArray = TK.Button(button_Frm,text= "Generate Random Array", command=window.destroy)
+    create_RandArray.grid(column=3,row=2)
+    
+    # window.destroy needs to change so that it starts the algorithmn checking 
+    # window.destroy just to place hold 
 
     linSearch = TK.BooleanVar(value=False)
     radSort = TK.BooleanVar(value=False)
@@ -21,13 +38,23 @@ def buildWindow():
     bubbSort = TK.BooleanVar(value=False)
     mergeSort = TK.BooleanVar(value=False)
 
-    TK.Checkbutton(frm, text="Linear Search", variable=linSearch).grid(column=1, row= 5)
-    TK.Checkbutton(frm, text="Radix Sort", variable=radSort).grid(column=2, row= 5)
-    TK.Checkbutton(frm, text="Quick Sort", variable=quickSort).grid(column=3, row= 5)
-    TK.Checkbutton(frm, text="Bubble Sort", variable=bubbSort).grid(column=4, row= 5)
-    TK.Checkbutton(frm, text="Merge Sort", variable=mergeSort).grid(column=5, row= 5)
+    TK.Checkbutton(button_Frm, text="Linear Search", variable=linSearch).grid(column=1, row= 5)
+    TK.Checkbutton(button_Frm, text="Radix Sort", variable=radSort).grid(column=2, row= 5)
+    TK.Checkbutton(button_Frm, text="Quick Sort", variable=quickSort).grid(column=3, row= 5)
+    TK.Checkbutton(button_Frm, text="Bubble Sort", variable=bubbSort).grid(column=4, row= 5)
+    TK.Checkbutton(button_Frm, text="Merge Sort", variable=mergeSort).grid(column=5, row= 5)
+    
+    
+    # TK.Text(frm).grid(column=3,row=2)
+    array = TK.Text(entry_Frm, height=1, width=20, wrap="word")
+    array.pack(side="left", fill="both", expand=True, padx=50, pady=100)
+    
+    # Scrollbar
+    scrollbar = TK.Scrollbar(entry_Frm, command=array.yview)
+    scrollbar.pack(side="right", fill="y")
+    array.config(yscrollcommand=scrollbar.set)
 
-    TK.Entry(frm).grid(column=3,row=2)
+
 
     window.mainloop()
 
